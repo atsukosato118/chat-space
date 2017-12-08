7b619b3bd2211bfad40cdd8d56a0425bff8c062d
atukosato <atsuko.stao118@gmail.com>
2017-12-08 20:09:37 +0900

    Add explanatory text

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
 
-# 編集するデータの取得/edit画面に行く
+# edit画面に行くときのアクション/編集するデータの取得
 	  def edit
 	  @user = User.find(params[:id])
 	  end
 
-# 特定のデータを更新/updateボタンを押した時
+# updateボタンを押した時/特定のデータを更新
 	  def update
-	  # findメソッドで更新するデータを取得
-	  # binding.pry
+binding.pry
+	  # binding.pryをここでしたらupdateアクションが押された時に処理が止まる
 	  @user = User.find(params[:id])
 	  if @user.update(user_params)
 	  	redirect_to :root, notice: "ok"
@@ -22,3 +22,42 @@ class UsersController < ApplicationController
   		params.require(:user).permit(:name, :email)
   end
 end
+
+
+# 特定のIDのデータを取得したい
+# @user = User.find(params[:id])
+# Userテーブルの[:id]のデータ取得
+
+# if @user.update(user_params)
+# 	ここでprivateメソッドのdef user_paramsが呼び出される
+
+# ストロングパラメーター
+# 指定したキーを持つパラメーターのみを受け取るようにするもの
+# params.require(:user).permit(:name, :email)
+# ①requireでPOSTで受け取る値のキーを設定
+# ②permitで許可するカラムを設定(DBに入れるカラムを設定)
+
+# ビューから送られてきた情報のハッシュであるparamsの中から:name, :email,
+# というキーとそのバリューのセットだけを残した新たなハッシュを生成しています。
+# この新たなハッシュが、tweet_paramsメソッドの返り値となる
+
+# redirect_to :root controller経由でrootパスへ
+# notice: "ok" okの通知
+
+# render :edit controllerを経由しないでeditへ
+# (edit-update) (new-create)のようにワンセットの時はrenderを使用することが多い
+
+# binding.pryを記載の場所でした時rails c --sandboxで確認すると
+#  ※rails c --sandboxだとDBに値が保存されない
+
+# $ rails c --sandbox
+# b(main):001:0> user = User.new(name: "sato", email: "sasa@sas")
+# b(main):002:0> user.saveで保存
+# b(main):003:0> paramsで確認
+# utf8"=>"✓", "_method"=>"patch", "authenticity_token"=>"JdFV8lC+/q6kR2avBSUR8bsknhdkcFjeaDhATMAa3NOxltoY3xQw+Jgata2lPT1Sy/wtFo6hxiPqQfNGzMjO+A=="
+#  "user"=>{"name"=>"atsuko", "email"=>"atsuko@atsuko", "password"=>"", "password_confirmation"=>""}, "commit"=>"Update", "controller"=>"users", "action"=>"update", "id"=>"1"} permitted: false>
+# paramsの中身表示される
+ # b(main):003:0> params[:id]
+ # idの値を表示
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- # ルートパスの設定
  root  'messages#index'
  resources :users, only:[:edit, :update]
 end
+
+# ルートパスの設定
+# usersコントローラはedit,updateアクションしか使っていないので、onlyオプションでアクションの指定
+#パスはhttp://localhost:3000/rails/info/routesまたは$ rake routesで確認
+
+# Railsでは、7つの基本アクションへのリクエストが下記の通りデフォルトで設定されています。これらのリクエストは、
+# routes.rbにresources :ルーティングを設定したいコントローラ名と書くだけで反映させることができます
+
+# Railsのリソースの7つのアクション	役割
+# index	リソースの一覧を表示する。
+# show	リソースの内容を表示する。
+# new	リソースを追加する。
+# create	リソースを追加し、作成する。
+# edit	リソースを更新するためのフォームを表示する。
+# update	リソースを更新する。
+# destroy	リソースを削除する。
