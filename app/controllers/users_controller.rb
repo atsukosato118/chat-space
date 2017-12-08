d769620c6711a7f367fb1ec5a3a08abd6b6d6e0c
atukosato <atsuko.stao118@gmail.com>
2017-12-08 19:19:19 +0900

    Add action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,24 @@
 class UsersController < ApplicationController
 
-# 編集するデータの取得
+# 編集するデータの取得/edit画面に行く
 	  def edit
 	  @user = User.find(params[:id])
 	  end
 
-# 特定のデータを更新
+# 特定のデータを更新/updateボタンを押した時
 	  def update
 	  # findメソッドで更新するデータを取得
+	  # binding.pry
 	  @user = User.find(params[:id])
-
 	  if @user.update(user_params)
-	    redirect_to user_url(@user)
+	  	redirect_to :root, notice: "ok"
 	  else
-	    render 'edit'
+	  	render :edit
 	  end
-	end
+  end
+
+  private
+  def user_params
+  		params.require(:user).permit(:name, :email)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # user_name/emailにバリデーション
-  validates :user_name, uniqueness: true
-  validates :email, uniqueness: true
+  # name/emailにバリデーション
+  validates :name, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
 end
 
-#バリデーションはモデルにかく
+
+#バリデーションはモデルにかくまたはマイグレーションファイルにかく
+#ActiveRecordには多くのバリデーションヘルパーが準備(AppicatiomRecordはActiveRecordを継承)
+#バリデーションとはオブジェクトがDBに保存される前に、そのデータが正しいかどうかを検証する仕組み
+# validates uniqueness: true, 一意性制約を設定したカラムには同じ値を設定できなくなります
+#           presence: true, 空でないこと
