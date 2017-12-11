3886bf20ceb4fc4f24ec7df0701948daeb3314f7
atukosato <atsuko.stao118@gmail.com>
2017-12-11 17:30:08 +0900

    Add group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,35 @@
 class GroupsController < ApplicationController
 
   def new
+     @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to :root, notice: 'グループを作成しました'
+    else
+      flash[:alert]='グループの作成に失敗しました'
+      render :create
+    end
   end
 
   def edit
+    @group = group.find(params[:id])
   end
 
   def update
+    @group = group.find(params[:id])
+    if @group.save
+      redirect_to :root, notice: 'グループを作成しました'
+    else
+      render :edit, alart: 'グループの作成に失敗しました'
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, {groups_ids: []})
+    # params.require(:撮ってきたい).permit(:name, {groups_ids: []})
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,16 @@
-class Group < ApplicationRecord
-end
+ class Group < ApplicationRecord
+  	has_many :messages
+  	has_many :users, through: :group_users
+  	has_many :group_users
+  end
+
+  # ## groupsテーブル
+
+  # |Column|Type|Options|
+  # |------|----|-------|
+  # |name|string|null: false, unigue: true|
+
+  # ### Association
+  # - has_many :messages
+  # - has_many :users, through: :group_users
+  # - has_many :group_users
