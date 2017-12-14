6f00ce5ece21c46d4b465083a1c00696baf4d79b
atukosato <atsuko.stao118@gmail.com>
2017-12-14 15:08:00 +0900

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  'groups#index'
   resources :users, only:[:edit, :update]
-  resources :groups, only:[:index, :new, :create, :edit, :update]
+  resources :groups, only:[:index, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
 
 # ルートパスの設定
@@ -21,3 +23,13 @@ end
 # edit	リソースを更新するためのフォームを表示する。
 # update	リソースを更新する。
 # destroy	リソースを削除する。
+
+#ネストしているため、 group_messages POST   /groups/:group_id/messages(.:format)となる
+# ネストについて（例）
+#resources :clients
+# 生成されるルーティングは /clients/:idである（複数形だから）※単数系だと：idが含まれない
+# resources :clients do
+#   #ネストすると/clients/:id/password になり、クライアントのパスワード設定ページへのルーティングを行える
+#   resource :password
+# end
+# クライアントidが1番のアカウントのパスワードを設定するページを新規に作る時などに利用出来る
