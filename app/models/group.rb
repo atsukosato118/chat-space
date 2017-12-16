7071d52891a944affb0d751390e0de0fdbd7395d
atukosato <atsuko.stao118@gmail.com>
2017-12-16 21:18:42 +0900

    Add group model method

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,8 +4,19 @@
     has_many :messages
     has_many :users, through: :group_users
     has_many :group_users
-  end
 
+      class << self
+      def last_message_show(group)
+        if group.messages.last.try(:body).present? then
+         return group.messages.last.body
+        elsif group.messages.last.try(:image).present? then
+          return "画像が投稿されています"
+        else
+          return "メッセージがありません"
+        end
+      end
+    end
+end
 
   # ## groupsテーブル
 
