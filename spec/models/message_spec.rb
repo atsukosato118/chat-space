b7bb3ad63f30b2117e63f7b4c887cd1bb89f54aa
atukosato <atsuko.stao118@gmail.com>
2017-12-18 19:11:25 +0900

    Add model test

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,26 +4,36 @@ describe Message do
   describe '#create' do
     # メッセージがあれば保存できる
     it "is valid with a message " do
-      message = build(:message, body: "")
+      message = build(:message, body: nil)
       expect(message).to be_valid
     end
-    # # 画像があれば保存できる
-    # it "is valid with a image " do
-    #   message = build(:message, body: nil)
-    #   expect(message).to be_valid
-    # end
-    # # メッセージと画像があれば保存できる
-    # it "is valid with a image " do
-    #   message = build(:message)
-    #   expect(message).to be_valid
-    # end
-    # # メッセージも画像もないと保存できない
-    # it "is invalid without a message and image " do
-    #   message = build(:message, image: "")
-    #   message = build(:message, body: nil)
-    #   message.valid?
-    #   # expect(user.errors[:image]).to include("can't be blank")
-    # end
+    # 画像があれば保存できる
+    it "is valid with a image " do
+      message = build(:message, body: nil)
+      expect(message).to be_valid
+    end
+     # メッセージと画像があれば保存できる
+    it "is valid with a image " do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+    # メッセージも画像もないと保存できない→メッセージと画像があれあば保存できる
+    it "is valid with a message and image " do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+    # group_idが無いと保存できない
+    it "is valid without group_id " do
+      message = build(:message, group: nil)
+      message.valid?
+      expect(message.errors[:group]).not_to include("group can't be blank")
+    end
+    # user_idが無いと保存できない
+    it "is valid without user_id " do
+      message = build(:message, user: nil)
+      message.valid?
+      expect(message.errors[:user]).not_to include("user can't be blank")
+    end
   end
 
 end
