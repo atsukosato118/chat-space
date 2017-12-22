4d1eba11670a14062afee113c4f76a01523c5fc4
atukosato <atsuko.stao118@gmail.com>
2017-12-22 20:24:56 +0900

    Js閉じたぐの再修正

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -10,63 +10,62 @@ $(document).on('turbolinks:load', function(){
                 <a class="user-search-add chat-group-user__btn
                 chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
                 </div>`
-                // console.log(search_html);でHTMLファイルが取れるか確認
-    search_list.append(search_html);
+    search_list.append(search_html)
   }
+
   // userなしの処理
   function appendNoUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user_name">${user}</p>
                 </div>`
                 // ${user}のuserは引数
-    search_list.append(search_html);
+    search_list.append(search_html)
   }
+
    // ajax化するタイミング
   $("#user-search-field").on("keyup", function(e) {
-      // 同期通信を止めるて、入力された値を全てとる
+    // 同期通信を止めるて、入力された値を全てとる
     e.preventDefault();
-      var input = $("#user-search-field").val();
-      // console.log(input);でフォームに入力した値が出力されるか確認
-      // console.log(this);でformDataの中身を確認
+    var input = $("#user-search-field").val();
+    // console.log(input);でフォームに入力した値が出力されるか確認
+    // console.log(this);でformDataの中身を確認
 
-      // ajax化
-      $.ajax({
-        type: 'GET',
-        url: '/users',
-        data: {keyword: input},
-        dataType: 'json'
-      })
+    // ajax化
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: {keyword: input},
+      dataType: 'json'
+    })
 
-      // ajaxでjsonから値が返った後の処理
-      .done(function(users) {
-        // console.log(users)でjsonが返って来ているか？
-        // 前に検索して表示された結果を消す（しないと前の検索結果にどんどん足される）
-        $("#user-search-result").empty();
-        // jbuilderから送られてきた配列の情報(今回はusers)によって場合分け
+    // ajaxでjsonから値が返った後の処理
+    .done(function(users) {
+      // console.log(users)でjsonが返って来ているか？
 
+      // 前に検索して表示された結果を消す
+      $("#user-search-result").empty();
+      // jbuilderから送られてきた配列の情報(今回はusers)によって場合分け
         if (users.length !== 0) {
           users.forEach( function(user){
              appendUser(user);
           });
-          }
+        }
         else {
-            appendNoUser("該当するユーザーは存在しません");
-         }
-      })
-
-      .fail(function(data) {
-        alert('検索に失敗しました');
-      });
-
-    $(".chat-group-form__field.clearfix").on("click", "user-search-add", function() {
-
-      var user = $(this)
-      user_id = user.data('user-id')
-      user_name = user.data('user-name')
-        addMember(user_id, user_name)
-        $(this).parent().remove();
+          appendNoUser("該当するユーザーは存在しません");
+        }
     })
-  });
+    .fail(function(data) {
+      alert('検索に失敗しました');
+    });
+    // $(".chat-group-form__field.clearfix").on("click", "chat-group-user", function() {
+    //
+    //   var user = $(this)
+    //   user_id = user.data('user-id')
+    //   user_name = user.data('user-name')
+    //     addUser(user_id, user_name)
+    //     $(this).parent().remove();
+    // })
+  })
 });
 
 // $(document).on('turbolinks:load', function(){
