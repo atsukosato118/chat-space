3a7646825cf890278735e308ec69c53d0547c6a1
atukosato <atsuko.stao118@gmail.com>
2017-12-22 19:32:14 +0900

    jsタグ修正

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,39 +1,71 @@
+// turbolinksだけど1回目からjs読み込むように
 $(document).on('turbolinks:load', function(){
+  // 処理関数を定義
+  var search_list = ("#user-search-result");
 
+  function appendUser(user) {
+  // インクリメンタルサーチの時に追加するHTML
+    var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${ user.name }</p>
+                <a class="user-search-add chat-group-user__btn
+                chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+                </div>`
+    search_list.append(html);
+  }
+
+  // userなしの処理
+  function appendNoUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user_name">${user}</p>
+                </div>`
+                // ${user}のuserは引数
+    search_list.append(html);
+  }
+   // ajax化するタイミング
   $("#user-search-field").on("keyup", function(e) {
-  e.preventDefault();
-    var input = $("#user-search-field").val();
-    // console.log(input);でフォームに入力した値が出力されるか確認
-    // console.log(this);でformDataの中身を確認
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: {keyword: input},
-      dataType: 'json'
-    })
-    .done(function(users) {
-      // console.log(users)でjsonが帰って来ているか？
-      $("#user-search-field").empty();
-      if (users.length !== 0) {
+      // 同期通信を止めるて、入力された値を全てとる
+    e.preventDefault();
+      var input = $("#user-search-field").val();
+      // console.log(input);でフォームに入力した値が出力されるか確認
+      // console.log(this);でformDataの中身を確認
 
-        users.forEach( function(user){
-           appendUser(user);
-        });
-        }
+      // ajax化
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: {keyword: input},
+        dataType: 'json'
+      })
+
+      // ajaxでjsonから値が返った後の処理
+      .done(function(users) {
+        // console.log(users)でjsonが返って来ているか？
+        // 前に検索して表示された結果を消す（しないと前の検索結果にどんどん足される）
+        $("#user-search-result").empty();
+        // jbuilderから送られてきた配列の情報(今回はusers)によって場合分け
+
+        if (users.length !== 0) {
+          users.forEach( function(user){
+             appendUser(user);
+          });
+          }
         else {
-          appendNoUser("該当するユーザーは存在しません");
-        }
+            appendNoUser("該当するユーザーは存在しません");
+         }
+      })
+
+      .fail(function(data) {
+        alert('検索に失敗しました');
+      });
+
+    $(".chat-group-form__field.clearfix").on("click", "a", function() {
+
+      var user = $(this)
+      user_id = user.data('user-id')
+      user_name = user.data('user-name')
+        addUser(user_id, user_name)
+        $(this).parent().remove();
     })
-    .fail(function() {
-      alert('検索に失敗しました');
-    });
-    return false;
-    //   console.log('ajax化成功')
-    //   $("#user-search-field").empty();
-    //   if (users.length !== 0) {
-    //     users.forEach(function(user){
-    //       appendUser(user);
-    //     });
   });
 });
 
@@ -55,3 +87,17 @@ $(document).on('turbolinks:load', function(){
 //   data: { keyword: input },
 //   dataType: 'json'
 // })
+
+// $(".chat-group-form__field.clearfix").on("click", "a", function() {
+//   chat-group-form__field.clearfixのaタグをクリックすると
+// var user = $(this)のthisはaタグ
+
+// user_id = user.data('user-id')
+// user_name = user.data('user-name')は
+// var html = `<div class="chat-group-user clearfix">
+//             <p class="chat-group-user__name">${ user.name }</p>
+//             <a class="user-search-add chat-group-user__btn
+//             chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+//             </div>`
+//   $(this).parent()
+//   thisの親要素は<div class="chat-group-user clearfix">なのでこれをremoveして空に
